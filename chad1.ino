8b3a1c453c6f469fb8be1e6f142e44b24f5f688c
AmanGusain2003 <amangusain2003@gmail.com>
2022-12-06 15:33:22 +0530

    The chad1 code is fixed and values are tweaked.

--- chad1.ino
+++ chad1.ino
@@ -1,11 +1,11 @@
 #include <Servo.h> 
 
-const int motor_pin1 = 2;       //forward
-const int motor_pin2 = 2;       //reverse
-const int servo_pin = 2;
-const int forward_delay = 1000;  //delay required for moving the motors forward.
-const int servo_low = 20;    //servo low limit for the claw
-const int servo_high = 120;  //servo high limit for the claw
+const int motor_pin1 = 4;       //forward
+const int motor_pin2 = 3;       //reverse
+const int servo_pin = 7;
+const int forward_delay = 3500;  //delay required for moving the motors forward.
+const int servo_low = 80;    //servo low limit for the claw
+const int servo_high = 150;  //servo high limit for the claw
 const int claw_speed = 20;   //decrease the value to increase the claw speed
 Servo servo1;
 
@@ -15,7 +15,9 @@ void setup(){
     servo1.attach(servo_pin);
     Serial.begin(9600);
     while(!Serial){}
-    servo1.write(0); 
+    servo1.write(servo_low); 
+    digitalWrite(motor_pin1, LOW);
+    digitalWrite(motor_pin2, LOW);
 
 }
 void loop(){
@@ -26,6 +28,7 @@ void loop(){
 }
 
 void run_motor(int i){      //pass 1 for forward, 2 for backward
+    Serial.println("Running Motor ");
     if(i == 1){     //move forward
         digitalWrite(motor_pin1, HIGH);
         delay(forward_delay);
@@ -39,13 +42,14 @@ void run_motor(int i){      //pass 1 for forward, 2 for backward
 }
 
 void grip(int i){               //pass 1 to close grip, 2 to open grip
+    Serial.println("Running Servo");
     if(i == 1){     //grip close
         for(int i = servo_low; i <= servo_high; i+=3){
             servo1.write(i);
             delay(claw_speed);
         }
     } else if (i == 2){     //grip open
-        for(int i = servo_high; i >= servo_low; i+=3){
+        for(int i = servo_high; i >= servo_low; i-=3){
             servo1.write(i);
             delay(claw_speed);
         }
