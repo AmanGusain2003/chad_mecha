3a06b4d687d2ec948757bbfd9f53888c60e31027
AmanGusainChitkra <aman4814.be21@chitkara.edu.in>
2022-12-21 13:46:20 +0530

    This code is working fine

--- chad3.ino
+++ chad3.ino
@@ -3,27 +3,30 @@
 #include <Servo.h>
 
 // All pins
-const int motor_pin1 = 4; // move grip forward
-const int motor_pin2 = 3; // move grip reverse
-const int servo_pin = 7;
-const int pulley_pin1 = 9;        // move pulley down
-const int pulley_pin2 = 6;        // move pulley up
-const int motor_move_robot1 = 23; // move the robot forward
-const int motor_move_robot2 = 22; // move the robot backward
-const int ultrasonic1_trig = 3; //forward ultrasonic trig 
-const int ultrasonic1_echo = 3; //forward ultrasonic echo
-const int ultrasonic2_trig = 3; //backward ultrasonic trig
-const int ultrasonic2_echo = 3; //backward ultrasonic echo
+const int motor_pin1 = 8; // move grip forward
+const int motor_pin2 = 7; // move grip reverse
+const int servo_pin = 9;
+const int pulley_pin1 = 10;        // move pulley down
+const int pulley_pin2 = 11;        // move pulley up
+const int pulley_pin3 = 6;        // move pulley up
+const int pulley_pin4 = 5;        // move pulley up
 
-//***********
+const int motor_move_robot1 = 13; // move the robot forward * 
+const int motor_move_robot2 = 12; // move the robot backward
+const int ultrasonic1_trig = 22; //forward ultrasonic trig 
+const int ultrasonic1_echo = 23; //forward ultrasonic echo
+const int ultrasonic2_trig = 53; //backward ultrasonic trig
+const int ultrasonic2_echo = 52; //backward ultrasonic echo
+
+//*****
 // some values
-const int forward_delay = 3500; // delay required for moving the grip motors forward.
-const int pulley_delay = 3000;  // delay required to completely pull pulley down or up.
+const int forward_delay = 2300; // delay required for moving the grip motors forward.
+const int pulley_delay = 2000;  // delay required to completely pull pulley down or up.
 const int servo_low = 80;       // servo low limit for the claw
-const int servo_high = 150;     // servo high limit for the claw
-const int claw_speed = 20;      // **decrease the value to increase the claw speed**
-const int safe_distance = 20;   // safe distance for the ultrasonic (in cm)
-//*********
+const int servo_high = 120;     // servo high limit for the claw
+const int claw_speed = 20;      // decrease the value to increase the claw speed
+const int safe_distance = 30;   // safe distance for the ultrasonic (in cm)
+//***
 
 Servo servo1;
 
@@ -37,6 +40,8 @@ void setup()
     pinMode(ultrasonic2_trig, OUTPUT);
     pinMode(motor_move_robot1, OUTPUT);
     pinMode(motor_move_robot2, OUTPUT);
+    pinMode(pulley_pin1, OUTPUT);
+    pinMode(pulley_pin2, OUTPUT);
     servo1.attach(servo_pin);
     Serial.begin(9600);
     while (!Serial)
@@ -45,16 +50,25 @@ void setup()
     servo1.write(servo_low);
     digitalWrite(motor_pin1, LOW);
     digitalWrite(motor_pin2, LOW);
+    digitalWrite(motor_move_robot1, LOW);
+    digitalWrite(motor_move_robot2, LOW);
+    digitalWrite(pulley_pin1, LOW);
+    digitalWrite(pulley_pin2, LOW);
+
+
 }
 void loop()
 {
+  int distance = get_distance(ultrasonic2_echo,ultrasonic2_trig);
+
+  Serial.println(distance);
     move_grip_motor(1);
     grip(1);
     move_grip_motor(2);
-    move_pulley(2);
-    move_robot(1);
-
+//    grip(2);
     move_pulley(1);
+    move_robot(1);
+    move_pulley(2);
     move_grip_motor(1);
     grip(2);
     move_grip_motor(2);
@@ -62,16 +76,18 @@ void loop()
     move_robot(2);
     Serial.println("Demo ended.");
     while (1);
+
+    
 }
 
-//****************************All Functions****************
+//**********All Functions******
 
 int get_distance(int echo_pin, int trig_pin) // pass ultrasonic pins to get the distance
 {
     digitalWrite(trig_pin, LOW);
     delayMicroseconds(2);
     // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(echo_pin, HIGH);
+    digitalWrite(trig_pin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig_pin, LOW);
     // Reads the echoPin, returns the sound wave travel time in microseconds
@@ -91,14 +107,16 @@ void move_robot(int i)   // pass 1 to move forward, pass 2 to move backward
         while (move)
         {
 
-            int distance = get_distance(ultrasonic1_echo, ultrasonic1_trig);
-            if (distance >= 10)
+            int distance = get_distance(ultrasonic2_echo, ultrasonic2_trig);
+            if (distance >= safe_distance)
             {
-                digitalWrite(motor_move_robot1, HIGH);
+                analogWrite(motor_move_robot1, 170);
             }
-            else
+            else{
                 digitalWrite(motor_move_robot1, LOW);
-            delay(10);
+                move = false;
+                delay(10);
+            }
         }
     }
 
@@ -106,32 +124,35 @@ void move_robot(int i)   // pass 1 to move forward, pass 2 to move backward
     {
         while (move)
         {
-            int distance = get_distance(ultrasonic2_echo, ultrasonic1_trig);
-            if (distance >= 10)
+            int distance = get_distance(ultrasonic2_echo, ultrasonic2_trig);
+            if (distance >= safe_distance)
             {
-                digitalWrite(motor_move_robot2, HIGH);
+                analogWrite(motor_move_robot2, 170);
             }
-            else
+            else{
                 digitalWrite(motor_move_robot2, LOW);
-            delay(10);
+                move = false;
+                delay(10);
+            }
         }
     }
 }
 
 void move_grip_motor(int i)   // pass 1 for forward, 2 for backward
 { 
-    Serial.println("Running Motor ");
+    Serial.print("Running Motor ");
+    Serial.println(i);
     if (i == 1)
     { // move forward
-        digitalWrite(motor_pin1, HIGH);
-        delay(forward_delay);
-        digitalWrite(motor_pin1, LOW);
+        analogWrite(motor_pin1, 70);
+        delay(forward_delay - 500);
+        analogWrite(motor_pin1, 0);
     }
     else if (i == 2)
     { // move backward
-        digitalWrite(motor_pin2, HIGH);
+        analogWrite(motor_pin2, 70);
         delay(forward_delay);
-        digitalWrite(motor_pin2, LOW);
+        analogWrite(motor_pin2, 0);
     }
 }
 
@@ -158,17 +179,25 @@ void grip(int i)    // pass 1 to close grip, 2 to open grip
 
 void move_pulley(int i) // pass 1 to down the pulley, pass 2 to up the pulley
 { 
-    Serial.println("Moving pulley");
+    Serial.print("Moving pulley");
+    Serial.println(i);
     if (i == 1)
     { // pull down
-        digitalWrite(pulley_pin1, HIGH);
-        delay(pulley_delay);
-        digitalWrite(pulley_pin1, LOW);
+        analogWrite(pulley_pin1, 160);
+        analogWrite(pulley_pin4, 120);
+        delay(pulley_delay + 700);
+        analogWrite(pulley_pin1, 100);
+        analogWrite(pulley_pin4, 80);
+
     }
     else if (i == 2)
     { // pull up
-        digitalWrite(pulley_pin2, HIGH);
+        analogWrite(pulley_pin1, 0);
+        analogWrite(pulley_pin4, 0);
+        analogWrite(pulley_pin2, 40);
+        analogWrite(pulley_pin3, 40);
         delay(pulley_delay);
-        digitalWrite(pulley_pin2, LOW);
+        analogWrite(pulley_pin2, 0);
+        analogWrite(pulley_pin3, 0);
     }
 }
